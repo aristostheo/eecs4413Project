554d16de107526e245570b2bad39fd7885c3060f
aristos <aristos@my.yorku.ca>
2024-08-24 17:16:05 -0400

    updates

--- 4413FinalProject/src/main/java/controller/PaymentController.java
+++ 4413FinalProject/src/main/java/controller/PaymentController.java
@@ -12,7 +12,7 @@ import javax.ws.rs.core.Response;
 
 import model.PaymentRequest;
 import model.PaymentResponse;
-import model.OrderService;
+//import model.OrderService;
 import model.PaymentService;
 
 @Path("/payment")
@@ -22,7 +22,7 @@ public class PaymentController {
     private PaymentService paymentService;
 
     @Inject
-    private OrderService orderService;
+//    private OrderService orderService;
 
     @POST
     @Path("/process")
@@ -33,10 +33,11 @@ public class PaymentController {
 
         if (paymentSuccess) {
             try {
-                int orderId = orderService.createOrder(request.getAmount());
-                PaymentResponse response = new PaymentResponse("Order successfully placed!", request.getAmount(), orderId);
+//                int orderId = orderService.createOrder(request.getAmount());
+                int orderId = 1; // \TODO: replace with orderID of some orderService class
+            	PaymentResponse response = new PaymentResponse("Order successfully placed!", request.getAmount(), orderId);
                 return Response.ok(response).build();
-            } catch (SQLException e) {
+            } catch (Exception e) {
                 e.printStackTrace();
                 PaymentResponse response = new PaymentResponse("Order creation failed.", 0);
                 return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(response).build();
